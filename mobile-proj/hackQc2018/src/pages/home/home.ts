1bbd0db117ff3f206df90d30955678c7b926b94a
hnspn <bobmatt911@gmail.com>
2018-05-05 05:40:32 -0400

    linked the login page with the home page

--- mobile-proj/hackQc2018/src/pages/home/home.ts
+++ mobile-proj/hackQc2018/src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  username: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams) {
 
+    this.username = this.navParams.get('username');
   }
 
 }
