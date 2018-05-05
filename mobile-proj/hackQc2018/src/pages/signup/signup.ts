196f66e43c36921f55d63211f92503424bf3d539
hnspn <bobmatt911@gmail.com>
2018-05-05 13:13:54 -0400

    signup http request test

--- mobile-proj/hackQc2018/src/pages/signup/signup.ts
+++ mobile-proj/hackQc2018/src/pages/signup/signup.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Platform } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
+import { API_USERS_URL } from '../../constants';
+import { HttpClient } from '@angular/common/http';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +12,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    public platform: Platform,
+    public httpNative: HTTP,
+    public httpBrowser: HttpClient) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  signup(email: string, password: string, firstName: string, lastName: string, phone: string) {
+    if (this.platform.is('cordova')) {
+    this.httpNative.post(API_USERS_URL, {
+      email,
+      password,
+      firstName,
+      lastName,
+      phone
+    }, {})
+    .then(() => this.showAlert('user added',`the user ${email} has been added, you can now login`))
+    .catch((err) => {this.showAlert('error', err); console.error(err)})
+    } else {
+      this.httpBrowser.post(API_USERS_URL, {
+        email,
+        password,
+        firstName,
+        lastName,
+        phone
+      }, {}).toPromise().then(() => this.showAlert('user added',`the user ${email} has been added, you can now login`))
+      .catch((err) => {this.showAlert('error', err); console.error(err)})
+    }
+  }
+
+  showAlert(title: string, subTitle: string) {
+    let alert = this.alertCtrl.create({
+      title,
+      subTitle,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
